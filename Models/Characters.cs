d632b38eafaaf413c6f95a688d5ad7ad45c123e5
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-28 14:46:13 +0100

    setup character model

---
+++ Models/Characters.cs
@@ -0,0 +1,17 @@
+public class Characters
+{
+    public required int Id { get; set; }
+    public required string Stage { get; set; }
+    public required string Name { get; set; }
+    public required int HP { get; set; }
+    public required string Type { get; set; }
+    public required string Info { get; set; }
+    public required string AbilityOne { get; set; }
+    public required string AbilityTwo { get; set; }
+    public int DamageOne { get; set; }
+    public int DamageTwo { get; set; }
+    public required string Weak { get; set; }
+    public required string Resistance { get; set; }
+    public required string Retreat { get; set; }
+    public required string ExtraInfo { get; set; }
+}
