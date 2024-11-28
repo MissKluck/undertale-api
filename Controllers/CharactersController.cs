858565245b0fbc064cbb3f72db35e4809f9786be
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-28 15:14:38 +0100

    added setup to connect to a database

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -6,11 +6,22 @@ namespace undertale_api;
 
 public class _CharactersController : ControllerBase
 {
-
+    private readonly AppDbContext _context;
 
     private static List<Characters> characters = new List<Characters> {
         new Characters {Id =, Stage ="", Name = "", HP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
     };
+
+    public _CharactersController(AppDbContext context)
+    {
+        _context = context;
+        if (!_context.Characters.Any())
+        {
+            _context.Characters.AddRange(characters);
+            _context.SaveChanges();
+        }
+    }
+
 
     // create the GET endpoint
     [HttpGet]
