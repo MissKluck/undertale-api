756a69eb7c0b9fdbaa6a0d5984db74887f3c459b
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-29 11:59:56 +0100

    added attack, defense, lv and exp

--- Models/Characters.cs
+++ Models/Characters.cs
@@ -4,6 +4,10 @@ public class Characters
     public required string Stage { get; set; }
     public required string Name { get; set; }
     public required int HP { get; set; }
+    public required int AT { get; set; }
+    public required int DF { get; set; }
+    public string? LV { get; set; }
+    public int EXP { get; set; }
     public required string Type { get; set; }
     public required string Info { get; set; }
     public required string AbilityOne { get; set; }
