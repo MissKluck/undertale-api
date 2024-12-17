9353bb982c30c9b4a4206a88450eabeaed52c1f8
MissKluck <sigrid.b.kluck@gmail.com>
2024-12-17 13:31:08 +0100

    added some comments

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -9,12 +9,19 @@ public class _CharactersController : ControllerBase
     private readonly AppDbContext _context;
 
     private static List<Characters> characters = new List<Characters> {
+        // Main Characters
         new Characters {Id = 1, Stage = "Pacifist", Name = "Frisk", HP = 20, LV = "LV 1", AT = 0, DF = 0, EXP = 0, Type = "Normal", Info = "A human child who has fallen down into the undeground.", AbilityOne = "TALK", AbilityTwo = "SPARE", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
         new Characters {Id = 2, Stage = "Pacifist", Name = "sans.", HP = 1, AT = 1, DF = 1, Type = "Psychic", Info = "The easiest enemy. Can only deal 1 damage", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        new Characters {Id = 3, Stage ="Pacifist", Name = "Papyrus", HP = 680, AT = 20, DF = 20, EXP =, Type = "", Info = "He likes to say: 'Nyeh heh heh!'", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "Flirting", Resistance = "", ExtraInfo = "Loves spaghetti", Retreat = "" },
+        new Characters {Id = 3, Stage ="Pacifist", Name = "Papyrus", HP = 680, AT = 20, DF = 20, EXP =, Type = "", Info = "He likes to say: 'Nyeh heh heh!'", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "Flirting", Resistance = "", ExtraInfo = "Loves puzzles", Retreat = "" },
         new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
         new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
         new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
+        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
+        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
+        //Regular monsters
+        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
+        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
+        //NPCs
         new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
         new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
     };
