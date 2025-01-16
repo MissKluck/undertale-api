21a8c644b9f84182c11bc30f89317f0ff7b35e70
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-16 13:06:48 +0100

    removed unnecessary code

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -10,20 +10,9 @@ public class _CharactersController : ControllerBase
 
     private static List<Characters> characters = new List<Characters> {
         // Main Characters
-        new Characters {Id = 1, Stage = "Pacifist", Name = "Frisk", HP = 20, LV = "LV 1", AT = 0, DF = 0, EXP = 0, Type = "Normal", Info = "A human child who has fallen down into the undeground.", AbilityOne = "TALK", AbilityTwo = "SPARE", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        new Characters {Id = 2, Stage = "Pacifist", Name = "sans.", HP = 1, AT = 1, DF = 1, Type = "Psychic", Info = "The easiest enemy. Can only deal 1 damage", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        new Characters {Id = 3, Stage ="Pacifist", Name = "Papyrus", HP = 680, AT = 20, DF = 20, EXP =, Type = "", Info = "He likes to say: 'Nyeh heh heh!'", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "Flirting", Resistance = "", ExtraInfo = "Loves puzzles", Retreat = "" },
-        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        //Regular monsters
-        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        //NPCs
-        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
-        new Characters {Id =, Stage ="", Name = "", HP =, AT =, DF =, EXP =, Type = "", Info = "", AbilityOne = "", AbilityTwo = "", DamageOne =, DamageTwo =, Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
+        new Characters {Id = 1, Stage = "Pacifist", Name = "Frisk", HP = 20, LV = "LV 1", AT = 0, DF = 0, EXP = 0, Type = "Normal", Info = "A human child who has fallen down into the undeground.", Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
+        new Characters {Id = 2, Stage = "Pacifist", Name = "sans.", HP = 1, AT = 1, DF = 1, Type = "Psychic", Info = "The easiest enemy. Can only deal 1 damage",  Weak = "", Resistance = "", ExtraInfo = "", Retreat = "" },
+        new Characters {Id = 3, Stage ="Pacifist", Name = "Papyrus", HP = 680, AT = 20, DF = 20, EXP = 3, Type = "", Info = "He likes to say: 'Nyeh heh heh!'", Weak = "Flirting", Resistance = "", ExtraInfo = "Loves puzzles", Retreat = "" },
     };
 
     public _CharactersController(AppDbContext context)
@@ -53,7 +42,7 @@ public class _CharactersController : ControllerBase
         }
         _context.Add(_characters);
         _context.SaveChanges();
-        return CreatedAtAction(nameof(Post), new { id = _characters.Id, stage = _characters.Stage, name = _characters.Name, hp = _characters.HP, type = _characters.Type, info = _characters.Info, abilityone = _characters.AbilityOne, abilitytwo = _characters.AbilityTwo, damageone = _characters.DamageOne, damagetwo = _characters.DamageTwo, weak = _characters.Weak, resistance = _characters.Resistance, extrainfo = _characters.ExtraInfo, retreat = _characters.Retreat }, _characters);
+        return CreatedAtAction(nameof(Post), new { id = _characters.Id, stage = _characters.Stage, name = _characters.Name, hp = _characters.HP, type = _characters.Type, info = _characters.Info, weak = _characters.Weak, resistance = _characters.Resistance, extrainfo = _characters.ExtraInfo, retreat = _characters.Retreat }, _characters);
     }
 
 }
