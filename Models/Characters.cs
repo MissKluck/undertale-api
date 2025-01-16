c9c854e1265c6e5f268bc97d18e9d0567ded6f3c
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-16 13:25:41 +0100

    changed some stats to not required

--- Models/Characters.cs
+++ Models/Characters.cs
@@ -3,9 +3,9 @@ public class Characters
     public required int Id { get; set; }
     public required string Stage { get; set; }
     public required string Name { get; set; }
-    public required int HP { get; set; }
-    public required int AT { get; set; }
-    public required int DF { get; set; }
+    public int HP { get; set; }
+    public int AT { get; set; }
+    public int DF { get; set; }
     public string? LV { get; set; }
     public int EXP { get; set; }
     public required string Type { get; set; }
@@ -13,7 +13,7 @@ public class Characters
     public List<string> Abilities { get; set; }
     public List<string> Attacks { get; set; }
     public required string Affinity { get; set; }
-    public required string Resistance { get; set; }
-    public required string Retreat { get; set; }
+    public string? Resistance { get; set; }
+    public string? Retreat { get; set; }
     public required string ExtraInfo { get; set; }
 }
