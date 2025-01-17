ec916d94801b35131a7ffb568a55fad719698893
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-17 10:31:01 +0100

    updated the model

--- Models/Characters.cs
+++ Models/Characters.cs
@@ -4,9 +4,9 @@ public class Characters
     public required string Stage { get; set; }
     public required string Name { get; set; }
     public int HP { get; set; }
+    public string? LV { get; set; }
     public int AT { get; set; }
     public int DF { get; set; }
-    public string? LV { get; set; }
     public int EXP { get; set; }
     public required string Type { get; set; }
     public required string Info { get; set; }
