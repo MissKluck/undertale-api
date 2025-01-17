b12766c75fcb96fdfce9e9ddb68defe20311ffeb
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-17 11:46:32 +0100

    changed ints to string

--- Models/Characters.cs
+++ Models/Characters.cs
@@ -3,11 +3,11 @@ public class Characters
     public required int Id { get; set; }
     public required string Stage { get; set; }
     public required string Name { get; set; }
-    public int HP { get; set; }
+    public string? HP { get; set; }
     public string? LV { get; set; }
-    public int AT { get; set; }
-    public int DF { get; set; }
-    public int EXP { get; set; }
+    public string? AT { get; set; }
+    public string? DF { get; set; }
+    public string? EXP { get; set; }
     public required string Type { get; set; }
     public required string Info { get; set; }
     public List<string> Abilities { get; set; }
