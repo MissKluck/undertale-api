4aaffa29675316ad9cc891e2a13f34ee000f3086
MissKluck <sigrid.b.kluck@gmail.com>
2025-02-20 11:35:01 +0100

    added swagger cors etc

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.OpenApi.Models;
 
 namespace undertale_api;
 
@@ -7,66 +10,49 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        builder.Services.AddDistributedMemoryCache();
+        builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(30);
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+        });
 
+        // Add services to the container.
         builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Undertale API", Version = "v1" });
+        });
+        builder.Services.AddSession();
+
+        builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .WithExposedHeaders("Content-Disposition");
+                });
+            });
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
-
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddCors(options =>
-               {
-                   options.AddPolicy("AllowAll", builder =>
-                   {
-                       builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().WithExposedHeaders("Count-Disposition");
-                   });
-               });
-
-        builder.Services.AddCors(options =>
-                {
-                    options.AddDefaultPolicy(builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                    });
-                });
-
-        var app = builder.Build();
-
         // makes sure we use swagger
         if (app.Environment.IsDevelopment())
         {
-            app.UseSession();
             app.UseSwagger();
             app.UseSwaggerUI();
 
         }
 
-        app.UseCors(policy =>
-        policy.WithOrigins("http://localhost:5500") // Allow requests from the frontend
-          .AllowAnyHeader()
-          .AllowAnyMethod());
-
-        app.UseDefaultFiles();
-
-        app.UseCors("AllowAll");
-
         app.UseHttpsRedirection();
-
+        app.UseCors("AllowAll");
+        app.UseSession();
         app.UseAuthorization();
-
         app.MapControllers();
-
         app.Run();
     }
 }
