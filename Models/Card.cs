4b39e54d4dfd8b041f312ede1221a67b9f43de1a
MissKluck <sigrid.b.kluck@gmail.com>
2025-02-20 10:13:03 +0100

    added card model

---
+++ Models/Card.cs
@@ -0,0 +1,14 @@
+public class CardModel
+{
+    public string Name { get; set; }
+    public string Stage { get; set; }
+    public int HP { get; set; }
+    public int AT { get; set; }
+    public int DF { get; set; }
+    public string Type { get; set; }
+    public string ImageUrl { get; set; }
+    public List<string> Abilities { get; set; }
+    public List<string> Affinities { get; set; }
+    public string Info { get; set; }
+    public string ExtraInfo { get; set; }
+}
