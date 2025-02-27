d7d02b87ed455efcdd562a96d11e7a1b25ea6c32
MissKluck <sigrid.b.kluck@gmail.com>
2025-02-27 11:19:44 +0100

    added missing requirements in the cardmodel

--- Models/Card.cs
+++ Models/Card.cs
@@ -3,12 +3,15 @@ public class CardModel
     public string? Name { get; set; }
     public string? Stage { get; set; }
     public int HP { get; set; }
+    public int LV { get; set; }
     public int AT { get; set; }
     public int DF { get; set; }
+    public int EXP { get; set; }
     public string? Type { get; set; }
     public string? ImageUrl { get; set; }
     public List<string>? Abilities { get; set; }
     public List<string>? Affinities { get; set; }
+    public List<string>? Attack { get; set; }
     public string? Info { get; set; }
     public string? ExtraInfo { get; set; }
 }
